8a94a5bdaa57053a5e251fec8d6fae0ad6e68858
SAEED HASSAN <saeedhisbani@yahoo.co.uk>
2023-10-10 15:02:32 +0500

    We resolved conficts and REBASE origin/main on local main Function 1 of user No 1

--- mainprog.py
+++ mainprog.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 def main():
     print('main function is made by user 1 and pushed to github.com')
+    func1user1()
     funcuser2()
+
+def func1user1():
+    print('Function 1 of user No 1')
 
 def funcuser2():
     print('Function 1 of user No 2')
+
+
 main()
