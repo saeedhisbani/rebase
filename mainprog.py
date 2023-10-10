69687be9ac24989975dc03ff4fdc1ca5448a382e
SAEED HASSAN <saeedhisbani@yahoo.co.uk>
2023-10-10 14:52:09 +0500

    From user 2 First Function is Added and pushed to github.com

--- mainprog.py
+++ mainprog.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 def main():
     print('main function is made by user 1 and pushed to github.com')
+    funcuser2()
 
-
+def funcuser2():
+    print('Function 1 of user No 2')
 main()
