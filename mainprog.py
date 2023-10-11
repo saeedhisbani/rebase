f04eaf34c38f5b9d4501df52f3325c336ec33c2e
SAEED <saeedhisbani@yahoo.co.uk>
2023-10-11 06:29:03 +0500

    function 3 of user 2

--- mainprog.py
+++ mainprog.py
@@ -3,6 +3,8 @@ def main():
     print('main function is made by user 1 and pushed to github.com')
     func1user1()
     funcuser2()
+    func2user2()
+    func3user2()
 
 def func1user1():
     print('Function 1 of user No 1')
@@ -10,5 +12,9 @@ def func1user1():
 def funcuser2():
     print('Function 1 of user No 2')
 
+def func2user2():
+    print('Function 2 of user No 2')
+def func3user2():
+    print('Function 3 of user No 2')
 
 main()
