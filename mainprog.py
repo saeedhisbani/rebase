c32f740f50edafc1075b7d128c9ca452407d3383
SAEED <saeedhisbani@yahoo.co.uk>
2023-10-11 06:36:19 +0500

    Applied $ git rebase --continue command after manually resolve all CONFICTS by editing mainprog.py file user no 1 function 2

--- mainprog.py
+++ mainprog.py
@@ -3,11 +3,18 @@ def main():
     print('main function is made by user 1 and pushed to github.com')
     func1user1()
     funcuser2()
+#<<<<<<< HEAD
     func2user2()
     func3user2()
+#=======
+    func2user1()
+#>>>>>>> 823f1f2 (user no 1 function 2)
 
 def func1user1():
     print('Function 1 of user No 1')
+
+def func2user1():
+    print('Function 2 of user No 1')
 
 def funcuser2():
     print('Function 1 of user No 2')
